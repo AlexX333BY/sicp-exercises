856aae53802249330a9d312b2154ab23c7a1dfe6
AlexX333BY <alexx333by@gmail.com>
2018-07-01 12:21:03 +0300

    -Part 1 completed

---
+++ part1/1.3.scm
@@ -0,0 +1,16 @@
+#lang scheme
+(define (square x) (* x x))
+
+(define (get-sum-of-squares x y) (+ (square x) (square y)))
+
+(define (get-max-of-3 x y z) (cond ((and (> x y) (> x z)) x)
+                                   ((and (> y x) (> y z)) y)
+                                   (else z)))
+
+(define (get-mid-of-3 x y z) (cond ((or (= x y) (= x z)) x)
+                                   ((= y z) y)
+                                   ((or (and (> x y) (< x z)) (and (< x y) (> x z))) x)
+                                   ((or (and (> y x) (< y z)) (and (< y x) (> y z))) y)
+                                   (else z)))
+
+(define (get-sum-of-squares-of-2-max x y z) (get-sum-of-squares (get-max-of-3 x y z) (get-mid-of-3 x y z)))
