96328ff96ff9a099f02153111afb4f6f7e1941bf
Aliaksei <AlexX333BY@gmail.com>
2018-07-15 14:45:23 +0300

    1.17 complete

---
+++ Chapter_1/1.17.scm
@@ -0,0 +1,16 @@
+(define (double n)
+  (* n 2))
+  
+(define (halve n)
+  (quotient n 2))
+  
+(define (fast-* a b)
+  (define (fast-*-iter a b t)
+    (cond ((= b 0) 0)
+          ((= b 1) (+ a t))
+          ((even? b) (fast-*-iter (double a) (halve b) t))
+          (else (fast-*-iter (double a) (halve (- b 1)) (+ a t)))))
+  (cond ((or (= a 0) (= b 0)) 0)
+        ((= a 1) b)
+        ((= b 1) a)
+        (else (fast-*-iter a b 0))))
