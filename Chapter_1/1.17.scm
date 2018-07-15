40eb673f3b828ce36defa8cb928192d394aa1a06
Aliaksei <AlexX333BY@gmail.com>
2018-07-15 15:10:28 +0300

    1.17 and 1.18 complete

---
+++ Chapter_1/1.17.scm
@@ -0,0 +1,10 @@
+(define (double n)
+  (* n 2))
+
+(define (halve n)
+  (quotient n 2))
+
+(define (fast-* a b)
+  (cond ((= b 0) 0)
+        ((even? b) (double (fast-* a (halve b))))
+        (else (+ a (fast-* a (- b 1))))))
